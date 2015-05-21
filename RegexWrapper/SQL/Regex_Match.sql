e567180ac3abe07c6af00b78671a3af63afded6c
churupaha <suhoborov@hotmail.com>
2015-05-21 11:00:56 +0400

    Исправлено форматирование в Regex_Matches.sql. Исправлен .gitignore. Добавлен файл Regex_Match.sql.

---
+++ RegexWrapper/SQL/Regex_Match.sql
@@ -0,0 +1,8 @@
+﻿create function [ext].[Regex_Match]
+(
+	@input nvarchar (max), 
+	@pattern nvarchar (4000), 
+	@options nvarchar (4000)
+)
+returns nvarchar(max)
+as external name [RegexWrapper].[UserDefinedFunctions].[Regex_Match]
