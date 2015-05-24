fe656dd2476d3e3e2c2705469f8a5959e8301434
churupaha <suhoborov@hotmail.com>
2015-05-24 21:18:38 +0400

    Рефакторинг для обратной совместимости.

--- RegexWrapper/RegexWrapper.cs
+++ RegexWrapper/RegexWrapper.cs
@@ -11,14 +11,205 @@ using Microsoft.SqlServer.Server;
 using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
-public partial class __Regex
+public class __Regex
 {
+    #region Обертка для static Regex Split
+
+    [SqlFunction(
+        IsDeterministic = true,
+        IsPrecise = true,
+        FillRowMethodName = "__Regex_SplitFill",
+        TableDefinition = "[part] nvarchar(max)")]
+    public static IEnumerable Regex_Split(
+        [SqlFacet(MaxSize = -1)] SqlString input,
+        SqlString pattern,
+        SqlString options)
+    {
+        if (pattern.IsNull)
+            throw new ArgumentException("Parameter [pattern] can not be NULL");
+
+        if (input.IsNull)
+            return new string[] { };
+
+        return
+            (IEnumerable)Regex.Split
+            (
+                input.Value,
+                pattern.Value,
+                __ParseRegexOptions(options)
+            );
+    }
+
+    public static void __Regex_SplitFill(
+        object obj,
+        [SqlFacet(MaxSize = -1)] out SqlString part)
+    {
+        part = new SqlString((string)obj);
+    }
+
+    #endregion Обертка для static Regex Split
+
+    #region Обертка для static Regex Matches
+
+    [SqlFunction(
+        IsDeterministic = true,
+        IsPrecise = true,
+        FillRowMethodName = "__Regex_MatchesFill",
+        TableDefinition = "[match] int, [group_name] nvarchar(4000), [group] int, [capture] int, [position] int, [length] int, [value] nvarchar(max)")]
+    public static IEnumerable Regex_Matches(
+        [SqlFacet(MaxSize = -1)] SqlString input,
+        SqlString pattern,
+        SqlString options)
+    {
+        if (pattern.IsNull)
+            throw new ArgumentException("Parameter [pattern] can not be NULL");
+
+        if (input.IsNull)
+            yield break;
+
+        Regex r = new Regex(pattern.Value, __ParseRegexOptions(options));
+
+        int mNum = 0;
+        foreach (Match m in r.Matches(input.Value))
+        {
+            int gNum = 0;
+            foreach (Group g in m.Groups)
+            {
+                string gName = r.GroupNameFromNumber(gNum);
+
+                int cNum = 0;
+                foreach (Capture c in g.Captures)
+                {
+
+                    yield return new
+                        __Match()
+                    {
+
+                        Match = mNum,
+                        GroupName = gName,
+                        Group = gNum,
+                        Capture = cNum,
+                        Position = c.Index,
+                        Length = c.Length,
+                        Value = c.Value
+                    };
+
+                    cNum++;
+                }
+
+                gNum++;
+            }
+
+            mNum++;
+        }
+    }
+
+    public static void __Regex_MatchesFill(
+        object obj, out SqlInt32 match, out SqlString groupName, out SqlInt32 group,
+        out SqlInt32 capture, out SqlInt32 position, out SqlInt32 length,
+        [SqlFacet(MaxSize = -1)] out SqlString value)
+    {
+        var m = (__Match)obj;
+
+        match = m.Match;
+        groupName = m.GroupName;
+        group = m.Group;
+        capture = m.Capture;
+        position = m.Position;
+        length = m.Length;
+        value = m.Value;
+    }
+
+    #endregion Обертка для static Regex Matches
+
+    #region Обертка для static Regex Match
+
+    [return: SqlFacet(MaxSize = -1)]
+    [SqlFunction(IsDeterministic = true, IsPrecise = true)]
+    public static SqlString Regex_Match(
+        [SqlFacet(MaxSize = -1)] SqlString input,
+        SqlString pattern,
+        SqlString options)
+    {
+        if (pattern.IsNull)
+            throw new ArgumentException("Parameter [pattern] can not be NULL");
+
+        if (input.IsNull)
+            return String.Empty;
+
+        return
+            Regex.Match
+            (
+                input.Value,
+                pattern.Value,
+                __ParseRegexOptions(options)
+            ).Value;
+    }
+
+    #endregion Обертка для static Regex Match
+
+    #region Обертка для static Regex IsMatch
+
+    [SqlFunction(IsDeterministic = true, IsPrecise = true)]
+    public static SqlBoolean Regex_IsMatch(
+        [SqlFacet(MaxSize = -1)] SqlString input,
+        SqlString pattern,
+        SqlString options)
+    {
+        if (pattern.IsNull)
+            throw new ArgumentException("Parameter [pattern] can not be NULL");
+
+        if (input.IsNull)
+            return false;
+
+        return
+            Regex.IsMatch
+            (
+                input.Value,
+                pattern.Value,
+                __ParseRegexOptions(options)
+            );
+    }
+
+    #endregion Обертка для static Regex IsMatch
+
+    #region Обертка для static Regex Replace
+
+    [return: SqlFacet(MaxSize = -1)]
+    [SqlFunction(IsDeterministic = true, IsPrecise = true)]
+    public static SqlString Regex_Replace(
+        [SqlFacet(MaxSize = -1)] SqlString input,
+        SqlString pattern,
+        [SqlFacet(MaxSize = -1)] SqlString replacement,
+        SqlString options)
+    {
+        if (pattern.IsNull)
+            throw new ArgumentException("Parameter [pattern] can not be NULL");
+
+        if (replacement.IsNull)
+            throw new ArgumentException("Parameter [replacement] can not be NULL");
+
+        if (input.IsNull)
+            return new SqlString(null);
+
+        return
+            Regex.Replace
+            (
+                input.Value,
+                pattern.Value,
+                replacement.Value,
+                __ParseRegexOptions(options)
+            );
+    }
+
+    #endregion Обертка для static Regex Replace
+
     #region Обертка для Split
     
     [SqlFunction(
         IsDeterministic = true,
         IsPrecise = true,
-        FillRowMethodName = "__Regex_SplitFill", 
+        FillRowMethodName = "__SplitFill", 
         TableDefinition = "[part] nvarchar(max)")]
     public static IEnumerable Split(SqlInt64 handle, [SqlFacet(MaxSize = -1)] SqlString input)
     {
@@ -33,7 +224,7 @@ public partial class __Regex
         return r.Split(input.Value);
     }
 
-    public static void __Regex_SplitFill(
+    public static void __SplitFill(
         object obj, 
         [SqlFacet(MaxSize = -1)] out SqlString part)
     {
@@ -47,7 +238,7 @@ public partial class __Regex
     [SqlFunction(
         IsDeterministic = true,
         IsPrecise = true, 
-        FillRowMethodName = "__Regex_MatchesFill",
+        FillRowMethodName = "__MatchesFill",
         TableDefinition = "[match] int, [group_name] nvarchar(4000), [group] int, [capture] int, [position] int, [length] int, [value] nvarchar(max)")]
     public static IEnumerable Matches(SqlInt64 handle, [SqlFacet(MaxSize = -1)] SqlString input)
     {
@@ -94,7 +285,7 @@ public partial class __Regex
         }
     }
 
-    public static void __Regex_MatchesFill(
+    public static void __MatchesFill(
         object obj, out SqlInt32 match, out SqlString groupName, out SqlInt32 group,
         out SqlInt32 capture, out SqlInt32 position, out SqlInt32 length, 
         [SqlFacet(MaxSize = -1)] out SqlString value)
@@ -246,3 +437,4 @@ public partial class __Regex
 
     #endregion Хелперы
 }
+
